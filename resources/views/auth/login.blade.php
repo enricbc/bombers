9c3f219b954858c1d205c21c175decac40e9b934
enricbc <33874389+enricbc@users.noreply.github.com>
2018-05-05 19:23:36 +0200

    w

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                          <label for="identity" class="col-md-4 control-label">{{ __('Usuari') }}</label>
+                          <label for="identity" class="col-md-4 col-form-label text-md-right">{{ __('Usuari') }}</label>
 
                             <div class="col-md-6">
                               <input id="identity" type="identity" class="form-control" name="identity" value="{{ old('identity') }}" autofocus>
